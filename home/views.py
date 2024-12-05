5beae2e61c9c65eede27f8ce82c4e0521c840d7b
Сергей Мухин <serhiomv04@gmail.com>
2024-12-05 15:38:46 +0300

    Added pogination

--- home/views.py
+++ home/views.py
@@ -1,17 +1,24 @@
-# Create your views here.
-from django.http import HttpResponse
-from upload.models import Track
+from django.db.models import Q
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from upload.models import Track
 
 def index(request):
-    query = request.GET.get('search', '')
-    tracks = Track.objects.all()
+    # Получаем параметр поиска
+    search_query = request.GET.get('q', '')  # Параметр `q` из URL
 
-    if query:
-        tracks = tracks.filter(title__icontains=query)
+    # Фильтрация треков
+    if search_query:
+        tracks = Track.objects.filter(
+            Q(title__icontains=search_query) | Q(artist__icontains=search_query)  # Поиск по названию и артисту
+        )
+    else:
+        tracks = Track.objects.all()  # Если нет параметра поиска, отображаем все треки
 
-    context = {
-        'tracks': tracks,
-        'query': query
-    }
-    return render(request, 'home/index.html', context)
+    # Настройка пагинации (по 3 трека на страницу)
+    paginator = Paginator(tracks, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    # Передаем данные в шаблон
+    return render(request, 'home/index.html', {'page_obj': page_obj, 'search_query': search_query})
